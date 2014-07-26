cef5a4020005bd220752f5810e1cb86b63da6573
MasterZU <master.h2zu@gmail.com>
2014-07-26 02:33:20 +0200

    add PlotD0LBranchTurtle example

--- example.py
+++ example.py
@@ -5,13 +5,25 @@ from pylsys import *
 import turtle 
 
 # simple draw
-l = D0Lsystem('F', {'F': 'F+F'})
-l.step(2)
+l = D0Lsystem('F', {'F': 'F+F--F+F'})
+print l
+l.step(3)
 t = PlotD0LTurtle(angle=60, lsystem=l)
 t.draw().done()
 
 # draw_evolute
-t = PlotD0LTurtle(angle=60, lsystem=D0Lsystem('F', {'F': 'F-F++F-F'}))
+t = PlotD0LTurtle(angle=60, lsystem=D0Lsystem('F', {'F': 'F+F--F+F'}))
 t.draw_evolute(3)
 
+
+# simple branch draw
+l = D0Lsystem('F', {'F': 'F[+F][-F]F'})
+print l
+l.step(3)
+t = PlotD0LBranchTurtle(angle=12, lsystem=l)
+t.draw().done()
+
+# draw_evolute with branches
+t = PlotD0LBranchTurtle(angle=12, lsystem=D0Lsystem('F', {'F': 'F[+F][-F]F'}))
+t.draw_evolute(3)
 
